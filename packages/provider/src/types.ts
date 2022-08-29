2ab462cfa983d02cf55fe6a6847ba2078f4cdb02
zhangyouxin <zhangyouxingg@126.com>
2022-08-29 11:45:56 +0800

    refactor: change design of provider interface

--- packages/provider/src/types.ts
+++ packages/provider/src/types.ts
@@ -5,19 +5,53 @@ import {
   Transaction,
   TransactionWithStatus,
 } from "@ckb-lumos/base";
+import { FromInfo } from '@ckb-lumos/common-scripts';
 import { BI } from "@ckb-lumos/bi";
 
 export interface Provider {
+  /**
+   * get tip block number
+   */
   getBlockNumber(): Promise<number>;
+   /**
+   * get tx pool fee rate
+   */
   getFeeRate(): Promise<number>;
-  getBalance(address: string): Promise<BI>;
+   /**
+   * get CKB balance of given fromInfo
+   */
+  getBalance(fromInfo: FromInfo): Promise<BI>;
+   /**
+   * send a signed tx to chain
+   */
   sendTransaction(signedTransaction: Transaction): Promise<Hash>;
+   /**
+   * get block by block number
+   */
   getBlockByNumber(blockNumber: number): Promise<Block>;
+   /**
+   * get tx by tx hash
+   */
   getTransaction(transactionHash: string): Promise<TransactionWithStatus>;
+   /**
+   * wait until a tx is committed on chain
+   */
   waitForTransaction(transactionHash: string, timeout?: number): Promise<void>;
-  privKeyToLock(privKey: string): Script;
-  pubKeyToLock(pubKey: string): Script;
+  // TODO: removed due to security reason
+  // privKeyToLock(privKey: string): Script;
+  
+  // TODO: need more types such as OmniLock, PwLock ...
+  pubKeyToLock(pubKey: string, type: 'secp256_k1' | 'acp'): Script;
+   /**
+   * transform address to a lock
+   */
   addressToLock(address: string): Script;
+   /**
+   * transform a lock to an address
+   */
   lockToAddress(lock: Script): string;
-  getSUDTBalance(SUDTInfo: string | Script, address: string): Promise<BI>;
+   /**
+   * get UDT balance of given fromInfo and UDT info
+   */
+  getSUDTBalance(SUDTInfo: string | Script, fromInfo: FromInfo): Promise<BI>;
 }
